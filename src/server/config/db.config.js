b4f429a8317133e5b618c4789a82a878ca5d5d99
ndv2002 <ndvinh2110@gmail.com>
2024-04-09 11:23:19 +0700

    change db config

--- src/server/config/db.config.js
+++ src/server/config/db.config.js
@@ -1,8 +1,21 @@
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "12345",
+//   DB: "specialized_project",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "12345",
-  DB: "specialized_project",
+  HOST: "us-cluster-east-01.k8s.cleardb.net",
+  USER: "bb640c66535559",
+  PASSWORD: "7fe53b76",
+  DB: "heroku_fa775146554d665",
   dialect: "mysql",
   pool: {
     max: 5,
