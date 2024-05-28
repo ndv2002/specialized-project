5a329b657ecc33eef8ececf5d00fffbde70a97c2
ndv2002 <ndvinh2110@gmail.com>
2024-05-28 17:10:29 +0700

    update db

--- src/server/config/db.config.js
+++ src/server/config/db.config.js
@@ -11,16 +11,29 @@
 //     idle: 10000
 //   }
 // };
+// module.exports = {
+//   HOST: "us-cluster-east-01.k8s.cleardb.net",
+//   USER: "b90a58dea7840b",
+//   PASSWORD: "1e39325d",
+//   DB: "heroku_7fdfb091a8162d8",
+//   dialect: "mysql",
+//   pool: {
+//     max: 20,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 3000
+//   }
+// };
 module.exports = {
-  HOST: "us-cluster-east-01.k8s.cleardb.net",
-  USER: "b90a58dea7840b",
-  PASSWORD: "1e39325d",
-  DB: "heroku_7fdfb091a8162d8",
+  HOST: "db-mysql-sgp1-37553-do-user-16686291-0.c.db.ondigitalocean.com:25060",
+  USER: "doadmin",
+  PASSWORD: "AVNS_ztnIGOkqeN954jmyJV5",
+  DB: "defaultdb",
   dialect: "mysql",
   pool: {
-    max: 20,
+    max: 100,
     min: 0,
     acquire: 30000,
-    idle: 3000
+    idle: 10000
   }
 };
